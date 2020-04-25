8d094fe29d4cd486dbb807c14b2e3d1f6fe0b77f
Cao Quoc Dat <quocdat_cao@yahoo.com>
2020-04-25 19:50:56 +0700

    ttt

--- NhomXingfa/Controllers/ProjectsController.cs
+++ NhomXingfa/Controllers/ProjectsController.cs
@@ -4,6 +4,7 @@ using NhomXingfa.Models;
 using PagedList;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -17,7 +18,7 @@ namespace NhomXingfa.Controllers
         public ActionResult Index()
         {
             ProjectViewModel model = new ProjectViewModel();
-            model.lstCatetory= db.Categories.Where(c => c.IsActive == true && c.TypeCate == WebConstants.CategoryProduct).ToList();
+            model.lstCategory = db.Categories.Where(c => c.IsActive == true && c.TypeCate == WebConstants.CategoryProduct).ToList();
             model.lstNews= db.Blogs.Where(q => q.IsActive == true && q.TypeBlog == WebConstants.BlogNews).OrderByDescending(q => q.LastModify).Take(5).ToList();
             return View(model);
         }
@@ -43,6 +44,26 @@ namespace NhomXingfa.Controllers
                 return PartialView("~/Views/Projects/_partialIndex.cshtml", lstprod.ToList().ToPagedList(pageNumber ?? 1, pageSize ?? 2));
             }
             return View(lstprod.ToList().ToPagedList(pageNumber ?? 1, pageSize ?? 2));
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return View("NotFound", "Home");
+            }
+            ProjectViewModel model = new ProjectViewModel();
+            model.detail = db.Products.Where(p => p.IsActive == true && p.IsProduct == false && p.ProductID == id).FirstOrDefault();
+            model.detail.CountView = model.detail.CountView + 1;
+            db.Entry(model.detail).State = EntityState.Modified;
+            db.SaveChanges();
+            model.lstCategory= db.Categories.Where(c => c.IsActive == true && c.TypeCate == WebConstants.CategoryProduct).ToList();
+            model.lstImages = db.ProductImages.Where(i => i.ProductID == id).ToList();
+            var cateid = db.Products.Where(p => p.IsActive == true && p.IsProduct == false && p.ProductID == id).FirstOrDefault().CategoryID;
+            model.lstProjects1 = db.Products.Where(p => p.IsActive == true && p.IsProduct == false && p.CategoryID == cateid && p.ProductID != id).OrderByDescending(p => p.ProductID).Take(4).ToList();
+
+
+            return View(model);
         }
     }
 }
