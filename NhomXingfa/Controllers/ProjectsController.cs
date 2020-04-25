428089c8e88844e4fc14793a3dfdf00d773f25e4
Cao Quoc Dat <quocdat_cao@yahoo.com>
2020-04-25 11:10:40 +0700

    OK

---
+++ NhomXingfa/Controllers/ProjectsController.cs
@@ -0,0 +1,48 @@
+﻿using NhomXingfa.Areas.Quantri.Models.DataModels;
+using NhomXingfa.Areas.Quantri.Utilities;
+using NhomXingfa.Models;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NhomXingfa.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        XingFaEntities db = new XingFaEntities();
+        // GET: Projects
+        public ActionResult Index()
+        {
+            ProjectViewModel model = new ProjectViewModel();
+            model.lstCatetory= db.Categories.Where(c => c.IsActive == true && c.TypeCate == WebConstants.CategoryProduct).ToList();
+            model.lstNews= db.Blogs.Where(q => q.IsActive == true && q.TypeBlog == WebConstants.BlogNews).OrderByDescending(q => q.LastModify).Take(5).ToList();
+            return View(model);
+        }
+
+        public ActionResult _partialIndex(int? pageNumber, int? pageSize)
+        {
+
+            if (pageSize == -1)
+            {
+                pageSize = db.Products.Where(b => b.IsProduct == false && b.IsActive == true && b.ProductCode == "BST").ToList().Count;
+            }
+            ViewBag.PageSize = pageSize;
+
+            var lstprod = db.Products.Where(b => b.IsProduct == false && b.IsActive == true && b.ProductCode == "BST").ToList();
+
+            lstprod = lstprod.OrderByDescending(s => s.ProductID).ToList();
+
+            ViewBag.STT = pageNumber * pageSize - pageSize + 1;
+            int count = lstprod.ToList().Count();
+            ViewBag.TotalRow = count;
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView("~/Views/Projects/_partialIndex.cshtml", lstprod.ToList().ToPagedList(pageNumber ?? 1, pageSize ?? 2));
+            }
+            return View(lstprod.ToList().ToPagedList(pageNumber ?? 1, pageSize ?? 2));
+        }
+    }
+}
---
+++ NhomXingfa/Models/ProjectViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NhomXingfa.Models;
+using NhomXingfa.Areas.Quantri.Models.DataModels;
+
+namespace NhomXingfa.Models
+{
+    public class ProjectViewModel
+    {
+        public List<Product> lstProjects { get; set; }
+        public List<Category> lstCatetory { get; set; }
+        public List<Blog> lstNews { get; set; }
+    }
+}
